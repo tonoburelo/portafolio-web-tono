aaa8a8c63b6aaa32284f35fd27c2f85c622416f5
tonoburelo <tonoburelohernandez@gmail.com>
2025-11-28 20:33:43 -0600

    asdf

--- app.js
+++ app.js
@@ -17,7 +17,14 @@ app.get("/contact", (req, res) => res.render("contact"));
 app.get("/projects/projects-taller", (req, res) => res.render("projects/projects-taller"));
 app.get("/projects/projects-inpi", (req, res) => res.render("projects/projects-inpi"));
 app.get("/projects/projects-portafolio", (req, res) => res.render("projects/projects-portafolio"));
-app.get("/projects/projects-sensai", (req, res) => res.render("projects/projects-sensai"));
+app.get("/projects/projects-typingapp", (req, res) => res.render("projects/projects-typingapp"));
+app.get("/projects/projects-comportamiento_clientes", (req, res) => res.render("projects/projects-comportamiento_clientes"));
+app.get("/projects/projects-salario_ciencia_datos", (req, res) => res.render("projects/projects-salario_ciencia_datos"));
+app.get("/projects/projects-predecir_seguros", (req, res) => res.render("projects/projects-predecir_seguros"));
+app.get("/projects/projects-friends", (req, res) => res.render("projects/projects-friends"));
+app.get("/projects/projects-pizzeria", (req, res) => res.render("projects/projects-pizzeria"));
+app.get("/projects/projects-libros", (req, res) => res.render("projects/projects-libros"));
+
 // Servidor
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Servidor en http://localhost:${port}`));
